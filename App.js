a5e8e87ae7eeaa65d06e14675a43fc81f1ff0ec9
tarrinneal <tarrinneal@gmail.com>
2022-03-29 17:06:28 -0700

    button with ts

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-web';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [times, setTimes] = useState([]);
+  const buttonPress = async () => {
+    await setTimes([...times, new Date().toTimeString()]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        {times.map((time) => (
+          <Text>{time}</Text>
+        ))}
+        <StatusBar style='auto' />
+      </View>
+      <View style={styles.container}>
+        <Pressable
+          onPress={buttonPress}
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white',
+            },
+            styles.wrapperCustom,
+          ]}
+        >
+          {({ pressed }) => (
+            <Text style={styles.text}>{pressed ? 'Pressed!' : 'Press Me'}</Text>
+          )}
+        </Pressable>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#748',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#999',
   },
 });
